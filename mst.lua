a8baa7363569b5dfb5674f5fce0e525c56974c64
lfernandoetec <lfernandoetec@gmail.com>
2023-11-17 09:14:20 -0300

    Retornar o mapa com a Minimum Spanning Tree

--- mst.lua
+++ mst.lua
@@ -1,40 +1,40 @@
--- Função para calcular a distância entre dois pontos (posições) no mapa
-function distance(pos1, pos2)
-    return math.abs(pos1[1] - pos2[1]) + math.abs(pos1[2] - pos2[2])
-end
-
--- Função para encontrar o conjunto ao qual um elemento pertence
-function find(parent, i)
-    if parent[i] == i then
-        return i
-    end
-    return find(parent, parent[i])
-end
-
--- Função para unir dois conjuntos
-function union(parent, rank, x, y)
-    local xroot = find(parent, x)
-    local yroot = find(parent, y)
-
-    if rank[xroot] < rank[yroot] then
-        parent[xroot] = yroot
-    elseif rank[xroot] > rank[yroot] then
-        parent[yroot] = xroot
-    else
-        parent[yroot] = xroot
-        rank[xroot] = rank[xroot] + 1
-    end
-end
-
--- Função MST que recebe um conjunto de posições e cria uma Minimum Spanning Tree entre elas
+-- Função MST que recebe um conjunto de posições e modifica o mapa com a Minimum Spanning Tree
 function minimumSpanningTree(map, positions)
     local edges = {}
+
+    -- Função para calcular a distância entre dois pontos (posições) no mapa
+    local function distance(pos1, pos2)
+        return math.abs(pos1[1] - pos2[1]) + math.abs(pos1[2] - pos2[2])
+    end
+
+    -- Função para encontrar o conjunto ao qual um elemento pertence
+    local function find(parent, i)
+        if parent[i] == i then
+            return i
+        end
+        return find(parent, parent[i])
+    end
+
+    -- Função para unir dois conjuntos
+    local function union(parent, rank, x, y)
+        local xroot = find(parent, x)
+        local yroot = find(parent, y)
+
+        if rank[xroot] < rank[yroot] then
+            parent[xroot] = yroot
+        elseif rank[xroot] > rank[yroot] then
+            parent[yroot] = xroot
+        else
+            parent[yroot] = xroot
+            rank[xroot] = rank[xroot] + 1
+        end
+    end
 
     -- Criar uma lista de arestas com pesos entre todas as posições
     for i = 1, #positions do
         for j = i + 1, #positions do
             local dist = distance(positions[i], positions[j])
-            table.insert(edges, {dist = dist, src = i, dest = j})
+            table.insert(edges, { dist = dist, src = i, dest = j })
         end
     end
 
@@ -50,20 +50,35 @@ function minimumSpanningTree(map, positions)
         rank[i] = 0
     end
 
-    local mstEdges = {}
-
-    -- Construir a MST usando o algoritmo de Kruskal
+    -- Construir a MST usando o algoritmo de Kruskal e marcar as arestas no mapa
     for _, edge in ipairs(edges) do
         local x = find(parent, edge.src)
         local y = find(parent, edge.dest)
 
         if x ~= y then
-            table.insert(mstEdges, edge)
+            local srcPos = positions[edge.src]
+            local destPos = positions[edge.dest]
+
+            -- Marcar a aresta no mapa
+            while srcPos[1] ~= destPos[1] or srcPos[2] ~= destPos[2] do
+                map[srcPos[1]][srcPos[2]] = 2 -- Valor para marcar a Minimum Spanning Tree no mapa
+                if srcPos[1] < destPos[1] then
+                    srcPos[1] = srcPos[1] + 1
+                elseif srcPos[1] > destPos[1] then
+                    srcPos[1] = srcPos[1] - 1
+                end
+                if srcPos[2] < destPos[2] then
+                    srcPos[2] = srcPos[2] + 1
+                elseif srcPos[2] > destPos[2] then
+                    srcPos[2] = srcPos[2] - 1
+                end
+            end
+
             union(parent, rank, x, y)
         end
     end
 
-    return mstEdges
+    return map
 end
 
 -- Exemplo de utilização
@@ -88,10 +103,18 @@ local positions = {
     {8, 1}
 }
 
-local mst = minimumSpanningTree(mapa, positions)
+local mapWithMST = minimumSpanningTree(mapa, positions)
 
--- Imprimir as arestas da MST
-print("Minimum Spanning Tree:")
-for _, edge in ipairs(mst) do
-    print(edge.src, edge.dest, edge.dist)
+-- Função para imprimir o mapa
+local function printMap(map)
+    for i = 1, #map do
+        for j = 1, #map[i] do
+            io.write(map[i][j] .. " ")
+        end
+        io.write("\n")
+    end
 end
+
+-- Imprimir o mapa com a Minimum Spanning Tree
+print("Mapa com Minimum Spanning Tree:")
+printMap(mapWithMST)
