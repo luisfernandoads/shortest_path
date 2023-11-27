f0ed1ef2d26480347c15b5b77787972915044e6f
Luis Fernando Alves da Silva <luisfernandoads@gmail.com>
2023-11-27 02:48:36 -0300

    refatorar graficos

--- export graphics.py
+++ export graphics.py
@@ -4,7 +4,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-# Encontrar todos os arquivos .csv no diretório atual
+def gerar_grafico_barras(data, file_name, y_data1, y_data2, ylabel, title, legend_labels, save_filename, execution_option):
+    ind = np.arange(len(data))
+    width = 0.25
+
+    plt.figure(figsize=(10, 6))
+    plt.bar(ind, data[y_data1], width, label=legend_labels[0])
+    plt.bar(ind + width, data[y_data2], width, label=legend_labels[1])
+    plt.xlabel(f"Número da Execução \n \n Opções de execução: {execution_option}")
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.xticks(ind + width, data['execution'])
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig(save_filename, format='svg')
+    #plt.show()
+
 file_list = glob.glob('*.csv')
 
 if len(file_list) == 0:
@@ -12,58 +28,19 @@ if len(file_list) == 0:
     exit()
 
 for file_path in file_list:
-    file_name = os.path.splitext(file_path)[0]  # Obtém o nome do arquivo sem a extensão
-
-    # Carregar o arquivo CSV
+    file_name = os.path.splitext(file_path)[0]
     data = pd.read_csv(file_path)
 
-    # Criar um array de índices para as barras
-    ind = np.arange(len(data))
+    execution_option = data['execution_option'].iloc[0]  # Obtendo a primeira ocorrência de 'execution_option'
 
-    # Configurações do gráfico
-    width = 0.25
+    gerar_grafico_barras(data, file_name, 'dijkstra_time', 'aStarManhattan_time', 'Tempo de Execução (ms)',
+                         'Tempo de Execução dos Algoritmos \n', ['Dijkstra', 'A* (Manhattan)'],
+                         f'{file_name}_tempo_execucao_barras.svg', execution_option)
 
-    # Gerar gráfico para o tempo de execução e salvar em SVG
-    plt.figure(figsize=(10, 6))
-    plt.bar(ind, data['dijkstra_time'], width, label='Dijkstra')
-    plt.bar(ind + width, data['aStarManhattan_time'], width, label='A* (Manhattan)')
-    plt.xlabel('Execução')
-    plt.ylabel('Tempo de Execução')
-    plt.title('Tempo de Execução dos Algoritmos')
-    plt.xticks(ind + width, data['execution'])
-    # Posicionando a legenda fora do gráfico
-    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    plt.grid(True)
-    plt.tight_layout()
-    plt.savefig(f'{file_name}_tempo_execucao_barras.svg', format='svg')
-    #plt.show()
+    gerar_grafico_barras(data, file_name, 'dijkstra_weight', 'aStarManhattan_weight', 'Peso do Caminho',
+                         'Peso do Caminho dos Algoritmos \n', ['Dijkstra', 'A* (Manhattan)'],
+                         f'{file_name}_peso_caminho_barras.svg', execution_option)
 
-    # Gerar gráfico para o peso do caminho e salvar em SVG
-    plt.figure(figsize=(10, 6))
-    plt.bar(ind, data['dijkstra_weight'], width, label='Dijkstra')
-    plt.bar(ind + width, data['aStarManhattan_weight'], width, label='A* (Manhattan)')
-    plt.xlabel('Execução')
-    plt.ylabel('Peso do Caminho')
-    plt.title('Peso do Caminho dos Algoritmos')
-    plt.xticks(ind + width, data['execution'])
-    # Posicionando a legenda fora do gráfico
-    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    plt.grid(True)
-    plt.tight_layout()
-    plt.savefig(f'{file_name}_peso_caminho_barras.svg', format='svg')
-    #plt.show()
-
-    # Gerar gráfico para o número de arestas e salvar em SVG
-    plt.figure(figsize=(10, 6))
-    plt.bar(ind, data['dijkstra_edges'], width, label='Dijkstra')
-    plt.bar(ind + width, data['aStarManhattan_edges'], width, label='A* (Manhattan)')
-    plt.xlabel('Execução')
-    plt.ylabel('Número de Arestas')
-    plt.title('Número de Arestas dos Algoritmos')
-    plt.xticks(ind + width, data['execution'])
-    # Posicionando a legenda fora do gráfico
-    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    plt.grid(True)
-    plt.tight_layout()
-    plt.savefig(f'{file_name}_num_arestas_barras.svg', format='svg')
-    #plt.show()
+    gerar_grafico_barras(data, file_name, 'dijkstra_edges', 'aStarManhattan_edges', 'Número de Arestas',
+                         'Número de Arestas dos Algoritmos \n', ['Dijkstra', 'A* (Manhattan)'],
+                         f'{file_name}_num_arestas_barras.svg', execution_option)
